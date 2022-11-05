97eb09e2db9eba99c69cc111b9d3f31d0ad69aa3
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-11-05 16:03:11 +0300

    added a basic example how an exception in an IO can be neglected by Haskell if the value is not anywhere in the program Haskell just does not execute it therefore if there is an exception it will not have any effect

--- src/IOActions/IOinRealApps.hs
+++ src/IOActions/IOinRealApps.hs
@@ -1,1 +1,24 @@
+{-
+  Examples of how laziness work in IO
+  in Haskell
+-}
 module IOActions.IOinRealApps where
+
+-- | no output because screamintoVoid
+-- is not used
+unevaluatedIO :: IO ()
+unevaluatedIO = 
+  let screamIntoVoid = putStrLn "quack" 
+  in return ()
+
+lazyIODemo :: IO ()  
+lazyIODemo =  
+  let
+    sayHello :: IO ()
+    sayHello = putStrLn "heylooo"
+
+    raiseMathError :: IO Int
+    raiseMathError = putStrLn "there is a math error"
+                    >> return (1 `div` 0)
+  in raiseMathError >> sayHello
+    
