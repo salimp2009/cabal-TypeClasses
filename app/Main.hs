6b31c9b8d329c1d33e1ccbb0e7e66bbec90e1abc
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-11-05 23:56:05 +0300

    added example of unsafeIO and safeIO

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,11 @@ module Main where
 
 import IOActions.MappingIO ( processArgs )
 import TypeClasses.Motivation
+import IOActions.IOinRealApps
 
 main :: IO ()
 main = do
   processArgs
   print $ uniqueElems (==) [1, 1, 2, 3, 1, 4]
+  -- unsafeIO
+  safeIO
--- src/IOActions/IOinRealApps.hs
+++ src/IOActions/IOinRealApps.hs
@@ -3,6 +3,7 @@
   in Haskell
 -}
 module IOActions.IOinRealApps where
+import Text.Printf
 
 -- | no output because screamintoVoid
 -- is not used
@@ -21,4 +22,50 @@ lazyIODemo =
     raiseMathError = putStrLn "there is a math error"
                     >> return (1 `div` 0)
   in raiseMathError >> sayHello
-    
+
+makeAndReadFile :: Int -> IO String
+makeAndReadFile filenum =
+  let fname = printf "tmp/test/%d" filenum
+  in writeFile fname fname  >> readFile fname
+
+-- | this is supposes to give error "Too many files"
+-- but on Windows it gives error "invalid file name after 2046"
+unsafeIO :: IO ()
+unsafeIO =
+  let files =mapM makeAndReadFile [1..500] :: IO [String]
+  in files >>= (putStrLn . show)
+
+makeAndShow :: Int -> IO ()
+makeAndShow num =
+   makeAndReadFile num >>= putStrLn
+
+-- | this is safe because we makeandwrite one file   
+-- then read from file then show
+-- prev version was creating all of them together
+-- this version goes one by one
+safeIO :: IO ()   
+safeIO =
+    mapM_ makeAndShow [1..10]  
+
+
+
+-- >>>:t mapM  
+-- mapM :: (Traversable t, Monad m) => (a -> m b) -> t a -> m (t b)
+
+-- >>>:t mapM_
+-- mapM_ :: (Foldable t, Monad m) => (a -> m b) -> t a -> m ()
+
+-- >>>:t printf  
+-- printf :: PrintfType r => String -> r
+
+-- >>>:i PrintfType
+-- type PrintfType :: * -> Constraint
+-- class PrintfType t where
+--   spr :: String -> [UPrintf] -> t
+--   {-# MINIMAL spr #-}
+--   	-- Defined in ‘Text.Printf’
+
+-- >>>:i FilePath
+-- type FilePath :: *
+-- type FilePath = String
+--   	-- Defined in ‘GHC.IO’
